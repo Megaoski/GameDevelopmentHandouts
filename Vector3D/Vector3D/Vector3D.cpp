fc956ed4b8c5478828d757d8a5b91dbeb90e87b4
Megaoski <oscar_2596@hotmail.com>
2018-09-17 23:01:40 +0200

    Added all functions
    DIstance to vector might me wrong

--- Vector3D/Vector3D/Vector3D.cpp
+++ Vector3D/Vector3D/Vector3D.cpp
@@ -1,29 +1,6 @@
 #include <iostream>
 #include "Vector3D.h"
 
-
-//using namespace std;
-//
-//
-//int main()
-//{
-//	vec3<int> vector_1 = vec3<int>(6, 4, 7);
-//	vec3<int> vector_2 = vec3<int>(2, 1, 7);
-//	vec3<int> vector_3 = vec3<int>(6, 4, 7);
-//
-//	printf("vector 1 = %i, %i, %i\n", vector_1.x, vector_1.y, vector_1.z);
-//	printf("vector 2 = %i, %i, %i\n", vector_2.x, vector_2.y, vector_2.z);
-//	
-//
-//	vec3<int> vector_4 = vector_1 + vector_2;
-//
-//	cout << vector_4.x << "\n";
-//
-//
-//	system("pause");
-//	return 0;
-//}
-
 using namespace std;
 
 int main()
--- Vector3D/Vector3D/Vector3D.h
+++ Vector3D/Vector3D/Vector3D.h
@@ -23,6 +23,11 @@ public:
 	void operator-=(const Vec3D<T> &vec);
 	void operator=(const Vec3D<T> &vec);
 	bool operator==(const Vec3D<T> &vec);
+	float moduled();
+	void normalize();
+	void toZero();
+	bool isZero();
+	float distanceToVector(Vec3D<T> vec);
 
 public:
 
@@ -80,6 +85,52 @@ template<class T>
 bool Vec3D<T>::operator==(const Vec3D<T> &vec)
 {
 	return x == vec.x && y == vec.y && z == vec.z;
+};
+
+template<class T>
+
+float Vec3D<T>::moduled()
+{
+	return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+};
+
+template<class T>
+
+void Vec3D<T>::normalize()
+{
+	Vec3D<T> normalized = this;
+
+	x = x / normalized.moduled();
+	y = y / normalized.moduled();
+	z = z / normalized.moduled();
+};
+
+template<class T>
+
+void Vec3D<T>::toZero()
+{
+	x = 0;
+	y = 0; 
+	z = 0;
+};
+
+template<class T>
+
+bool Vec3D<T>::isZero()
+{
+	return x == 0 && y == 0 && z == 0;
+}
+
+template<class T>
+
+float Vec3D<T>::distanceToVector(Vec3D<T> vec)
+{
+	Vec3D<T> resultVec;
+	resultVec.x = x - vec.x;
+	resultVec.y = y - vec.y;
+	resultVec.z = z - vec.z;
+
+	return resultVec;
 };
 
 #endif
