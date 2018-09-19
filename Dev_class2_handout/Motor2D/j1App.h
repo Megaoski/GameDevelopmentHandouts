895f03cc86038e95750ef77ca79797f517955484
Megaoski <oscar_2596@hotmail.com>
2018-09-19 17:59:11 +0200

    Testing config.xml

--- Dev_class2_handout/Motor2D/j1App.h
+++ Dev_class2_handout/Motor2D/j1App.h
@@ -77,9 +77,9 @@ private:
 	float				dt;
 
 	// TODO 2: Create two new variables from pugui namespace:
-	// a xml_document to store the while config file and
+	// a xml_document to store the whole config file and
 	// a xml_node to read specific branches of the xml
-
+	/*pugi::xml_node*/
 	int					argc;
 	char**				args;
 };
