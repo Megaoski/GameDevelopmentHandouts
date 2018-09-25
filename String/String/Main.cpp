16d8c7ac24d9256f0c917ac7f2f86711799bcbe4
Megaoski <oscar_2596@hotmail.com>
2018-09-25 20:54:17 +0200

    Working on the String Class

--- String/String/Main.cpp
+++ String/String/Main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main()
 {
+	MyString example("Hello");
+
+	cout << example.text << endl;
 
 	system("pause");
 	return 0;
--- String/String/StringClass.h
+++ String/String/StringClass.h
@@ -1,12 +1,27 @@
 #ifndef STRING_CLASS
 #define STRING_CLASS
 
+typedef unsigned int uint;
+
 class MyString
 {
 public:
 
-	MyString() {}
+	MyString() : text(nullptr), characters(0) {}
+	MyString(const char* word) {}
 
+	virtual ~MyString() 
+	{
+		delete[] text;
+	}
+
+	//metodo para comprar dos triungs y ver si son iguales
+
+
+private:
+
+	uint characters = 0;
+	char* text = nullptr;
 
 };
 
